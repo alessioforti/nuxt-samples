8b8dcbf3007f2c67a22560d16a1427e0cea415a1
Alessio Forti <info@alessioforti.it>
2023-06-27 14:55:41 +0200

    auth middleware

--- pocketbase/pages/index.vue
+++ pocketbase/pages/index.vue
@@ -1,19 +1,24 @@
+<script setup>
+import PocketBase from 'pocketbase'
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+definePageMeta({
+	middleware: 'auth'
+})
+
+const logout = () => {
+	pb.authStore.clear()
+	navigateTo('/login')
+}
+</script>
 <template>
 	<div class="w-full h-screen grid place-content-center">
-		<div>
-			<NuxtLink to="/signup" class="btn">
-				Sign Up
-			</NuxtLink>
+		<div class="text-center">
+			<h2 class="text-2xl font-bold mb-10">Protected page</h2>
 
-			<NuxtLink to="/" class="btn">
-				Log In
-			</NuxtLink>
-
-			<p class="mt-6 text-sm text-gray-600 text-center underline hover:text-sky-700">
-				<NuxtLink to="/">
-					Forgot password?
-				</NuxtLink>
-			</p>
+			<button @click="logout" class="btn">
+				Logout
+			</button>
 		</div>
 	</div>
 </template>
